e76e742745f8436a72da010e1df604e09635d906
Kaillane Martins <kaillanemartins18@gmail.com>
2023-04-25 21:28:02 -0300

    Implementando com arquivos

--- src/arvore/TrieTest.java
+++ src/arvore/TrieTest.java
@@ -1,42 +1,28 @@
 package arvore;
 
+import util.Controlador;
+import util.LeituraArquivo;
+
 public class TrieTest {
     
 	public static void main(String[] args) {
         
         // Input keys (use only 'a' through 'z' and lower case)
-          String chaves[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
         
-          String mensagemDeSaida[] = {"Nao esta presente na trie", "Esta presente trie"};
-        
-          Trie trie = new Trie();
-          
-          for (int i = 0; i < chaves.length ; i++){
-              trie.insert(chaves[i]);
-          }    
-        
-          trie.remove("the");
+        LeituraArquivo dados = new LeituraArquivo("src\\entrada\\palavras.txt");
+        LeituraArquivo comandos = new LeituraArquivo("src\\entrada\\comandos.txt");
+        Controlador ctrl = new Controlador();
 
-          // -> Buscando por diferentes chaves
-          if( trie.search("the") == true)
-              System.out.println("the --- " + mensagemDeSaida[1]);
-          else System.out.println("the --- " + mensagemDeSaida[0]);
-           
-          if( trie.search("any") == true)
-              System.out.println("any --- " + mensagemDeSaida[1]);
-          else System.out.println("any --- " + mensagemDeSaida[0]);
-           
-          if( trie.search("their") == true)
-              System.out.println("their --- " + mensagemDeSaida[1]);
-          else System.out.println("their --- " + mensagemDeSaida[0]);
-           
-          if( trie.search("by") == true)
-              System.out.println("by --- " + mensagemDeSaida[1]);
-          else System.out.println("by --- " + mensagemDeSaida[0]);
-
-          if( trie.search("a") == true)
-              System.out.println("a --- " + mensagemDeSaida[1]);
-          else System.out.println("a --- " + mensagemDeSaida[0]);
-  
+        System.out.println("Kaillane Corrêa Martins");
+        System.out.println("----------Iniciando----------");
+        System.out.println("");
+        
+        ctrl.adicionarDados(dados.lerTudo());
+        
+        Object comando[] = comandos.lerTudo();
+        for (int i = 0; i < comando.length; i++) {
+            ctrl.tratar(comando[i].toString());
+        }
+        
       }
 }
